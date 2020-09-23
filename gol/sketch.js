aff6f4f27025235a408a25d2eb5a80a1ce594dfd
cautiouspotato <afshaanlondhe4@gmail.com>
2020-09-23 17:40:23 +0530

    it stopped drawing the triangles now

--- gol/sketch.js
+++ gol/sketch.js
@@ -4,18 +4,19 @@ let bHeight, bWidth, gState;
 const resolution = 10;
 
 function drawBox(x, y, color, size) {
-    stroke(150);
-    const c = color === 'W' ? 0 : 255;
+    noStroke();
+    const c = color === 'B' ? 0 : 255;
     fill(c);
     rect(x * size, y * size, size, size);
 }
 
-// count up immediate neighbors mmmm
 function cntNeighbors(grid, x, y) {
     let sum = 0;
     for (let i = -1; i < 2; i++) {
         for (let j = -1; j < 2; j++) {
-            sum += grid[x + 1][y + j];
+            let c = (x + i + bWidth) % bWidth;
+            let r = (y + j + bHeight) % bHeight;
+            sum += grid[c][r];
         }
     }
     sum -= grid[x][y];
@@ -23,45 +24,43 @@ function cntNeighbors(grid, x, y) {
 }
 
 function setup() {
-    createCanvas(800, 800);
-    bWidth = width / resolution;
-    bHeight = height / resolution;
+    createCanvas(displayWidth, displayHeight);
+    bWidth = round(width / resolution);
+    bHeight = round(height / resolution);
     // fill it with noise
     gState = [...Array(bWidth)].map(() => (
         [...Array(bHeight)].map(() => (
-            //(Math.random() % 2 < 0.09) ? 1 : 0
             Math.round(Math.random())
         ))
     ));
-
 }
 
 function draw() {
     background(255);
 
-    const output = gState;
+    // stupid
+    const next = [...Array(bWidth)].map(() => (new Array(bHeight)));
+
     // game of life game of life
-    for (let x = 1; x < bWidth - 1; x++) {
-        for (let y = 1; y < bHeight - 1; y++) {
-            const nNeighbours = cntNeighbors(output, x, y);
+    for (let x = 0; x < bWidth; x++) {
+        for (let y = 0; y < bHeight; y++) {
+            const nNeighbours = cntNeighbors(gState, x, y);
+            const state = gState[x][y];
 
             // set states
-            if (output[x][y] === 1) {
-                if (nNeighbours < 2 || nNeighbours > 3)
-                    gState[x][y] = 0;
+            if (state === 0 && nNeighbours === 3) {
+                next[x][y] = 1;
+            } else if (state == 1 && (nNeighbours < 2 || nNeighbours > 3)) {
+                next[x][y] = 0;
             } else {
-                if (nNeighbours === 3)
-                    gState[x][y] = 1;
+                next[x][y] = state;
             }
 
             // draw
-            if (output[x][y] === 1) {
-                drawBox(x, y, 'W', resolution);
-            } else {
+            if (state === 1) {
                 drawBox(x, y, 'B', resolution);
             }
         }
     }
-    //console.log(gState);
-    //console.table(output);
+    gState = next;
 }
