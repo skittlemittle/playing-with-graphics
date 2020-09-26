3084d43a0f4c56d628ae805fc96adfa7754fc481
cautiouspotato <afshaanlondhe4@gmail.com>
2020-09-26 14:32:22 +0530

    cooling map generator
    LEDS NOT LIGHTING!!!

--- fire/fire.ino
+++ fire/fire.ino
@@ -7,31 +7,44 @@
 #define LED_PIN 3
 #define COLOR_ORDER GRB
 #define CHIPSET WS2811
-#define BRIGHTNESS 16
+#define BRIGHTNESS 64
+#define FRAMERATE 1
 
 const uint8_t kMatrixWidth = 8;
 const uint8_t kMatrixHeight = 8;
 const bool kMatrixSerpentineLayout = true;
-#define NUM_LEDS (matrixWidth *  matrixHeight)
+#define NUM_LEDS (kMatrixWidth *  kMatrixHeight)
 CRGB leds_plus_safety_pixel[NUM_LEDS + 1];
 CRGB *const leds(leds_plus_safety_pixel + 1);
 // fire buffers fire buffers
-CHSV buffer1[NUM_LEDS];
-CHSV buffer2[NUM_LEDS];
-int scroll;
+int buffer1[NUM_LEDS];
+int buffer2[NUM_LEDS];
+int scroll = 0;
 // performance amirite
-int coolingMap [] = {}; // TODO
+int coolingMap [] = {
+    0,0,0,0,0,0,0,0,
+    0,13,34,30,15,33,1,0,
+    0,44,44,50,62,10,37,0,
+    0,32,76,71,34,71,3,0,
+    0,54,54,60,73,11,42,0,
+    0,22,57,48,24,49,2,0,
+    0,22,19,24,26,4,16,0,
+    0,0,0,0,0,0,0,0
+};
 
 uint16_t XYsafe(uint8_t x, uint8_t y);
 
 void setup()
 {
+    FastLED.setBrightness(BRIGHTNESS);
     // a line of "source fire" at the bottom
     for (size_t x = 0; x < kMatrixWidth; x++) {
         int y = kMatrixHeight - 1;
         int index = x + y * kMatrixWidth;
-        buffer1[index] = CHSV(32, 255, 255); // orang
+        buffer1[index] = 255;
+        buffer2[index] = 255;
     }
+    Serial.begin(9600);
 }
 
 void loop()
@@ -40,30 +53,34 @@ void loop()
     for (size_t x = 1; x < kMatrixWidth - 1; x++) {
         for (size_t y = 1; y < kMatrixHeight - 1; y++) {
             // the up, down, left, and right neighbors
-            int neighbor1 = buffer1[x + 1 + y * kMatrixWidth].val;
-            int neighbor2 = buffer1[x - 1 + y * kMatrixWidth].val;
-            int neighbor3 = buffer1[x + (y + 1) * kMatrixWidth].val;
-            int neighbor4 = buffer1[x + (y - 1) * kMatrixWidth].val;
+            int neighbor1 = buffer1[x + 1 + y * kMatrixWidth];
+            int neighbor2 = buffer1[x - 1 + y * kMatrixWidth];
+            int neighbor3 = buffer1[x + (y + 1) * kMatrixWidth];
+            int neighbor4 = buffer1[x + (y - 1) * kMatrixWidth];
 
             int newColor = (neighbor1 + neighbor2 + neighbor3 + neighbor4) * 0.25;
             newColor -= coolingMap[(x + (y + scroll) * kMatrixWidth)];
+            if (newColor < 0) newColor = 0;
+            if (newColor > 255) newColor = 255;
 
-            buffer2[x + (y - 1) * kMatrixWidth].val = newColor; // don't disturb source
+            buffer2[x + (y - 1) * kMatrixWidth] = newColor; // don't disturb source
         }
     }
 
     // draw
     for (size_t x = 0; x < kMatrixWidth; x++) {
         for (size_t y = 0; y < kMatrixHeight; y++) {
-            leds[(XYsafe(x, y))] = buffer2[x + y * kMatrixWidth];
+            byte brightness = buffer2[x + y * kMatrixWidth];
+            leds[(XYsafe(x, y))].setHSV(32, 255, brightness); //orange
         }
     }
-    FastLED.setBrightness(BRIGHTNESS);
-    FastLED.show();
     // swamp
-    buffer1 = buffer2;
+    memcpy(buffer1, buffer2, sizeof buffer2);
     scroll++;
     if (scroll > kMatrixHeight) scroll = 0;
+
+    FastLED.show();
+    delay(1000 / FRAMERATE);
 }
 
 /*
